21750a3d29e45cdc4c4c134870a9c632b11f8e45
Hellshi <hcslimaa@gmail.com>
2021-06-28 14:46:27 -0300

    Setting redux functions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
 
   const dispactch = useDispatch();
 
@@ -10,18 +11,26 @@ const Counter = () => {
     dispactch({ type: "ADD" });
   };
 
+  const IncreaseByFive = () => {
+    dispactch({ type: "increase", amount: 5 });
+  };
+
   const DecreaseHandler = () => {
     dispactch({ type: "DECREASE" });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispactch({ type: "TOGGLE" });
+    console.log(showCounter);
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={IncreaseHandler}>Increment</button>
+        <button onClick={IncreaseByFive}>Increase by 5</button>
         <button onClick={DecreaseHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/store/Redux-Index.js
+++ src/store/Redux-Index.js
@@ -1,15 +1,36 @@
 import { createStore } from "redux";
 
-const reducerCounter = (state = { counter: 0 }, action) => {
+const initialState = {
+  counter: 0,
+  showCounter: true,
+};
+
+const reducerCounter = (state = initialState, action) => {
   if (action.type === "ADD") {
     return {
       counter: state.counter + 1,
+      showCounter: initialState.showCounter,
+    };
+  }
+
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.amount,
+      showCounter: initialState.showCounter,
     };
   }
 
   if (action.type === "DECREASE") {
     return {
       counter: state.counter - 1,
+      showCounter: initialState.showCounter,
+    };
+  }
+
+  if (action.type === "TOGGLE") {
+    return {
+      counter: initialState.counter,
+      showCounter: !state.showCounter,
     };
   }
 
